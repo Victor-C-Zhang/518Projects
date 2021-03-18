787215064b2fa6144d1ccfe86d99a0921ec4e1ef
Bharti Mehta <bharu2999@gmail.com>
2021-03-17 21:16:38 -0400

    benchmark code

--- asst1/CODE/data.c
+++ asst1/CODE/data.c
@@ -23,10 +23,14 @@ typedef struct my_params {
 	my_pthread_mutex_t* lock;
 } my_params;
 
+FILE *fp;
 struct timeval start;
 struct timeval stop;
 int totalTime = 20;
-FILE *fp;
+int mult;
+int isLock;
+int threadNum;
+
 
 void* my_thread_func(void* args) {
   my_params* vals = (my_params*)args;
@@ -51,89 +55,75 @@ void* thread_func(void* args) {
   return (void*)30;
 }
 
-void my_pthread_data() {
-	fp=fopen("my_pthread_data.csv","w+");	
-	fprintf(fp,"Multiple, Synchronized, Number of threads, Avg time across %d runs\n", totalTime);
+int my_pthread_data() {
+	gettimeofday(&start, 0);
+	my_pthread_t threads[threadNum];
+	my_params* args[threadNum];
+	my_pthread_mutex_t lock;
+	my_pthread_mutex_init(&lock, NULL);
+	for (int i = 0; i < threadNum; i++){
+		args[i] = (my_params*) malloc(sizeof(my_params));
+		args[i] -> lock = &lock;
+		args[i] -> multiple = mult * .25f;
+		args[i] -> isLock = isLock;
+		my_pthread_create(&threads[i], NULL, my_thread_func, (void*)args[i]);
+	}
+	for (int i = 0; i < threadNum; i++) {
+		my_pthread_join(threads[i], NULL);
+	}	
+	for (int i = 0; i < threadNum; i++) {
+		free(args[i]);
+	}
+	my_pthread_mutex_destroy(&lock);
+	gettimeofday(&stop, 0);
+	return (( stop.tv_sec-start.tv_sec)*1000000 + stop.tv_usec-start.tv_usec);
+}
+
+int pthread_data() {
+	gettimeofday(&start, 0);
+	pthread_t threads[threadNum];
+	pthread_attr_t threadAttrs;
+	pthread_attr_init(&threadAttrs);
+	params* args[threadNum];
+	pthread_mutex_t lock;
+	pthread_mutex_init(&lock, NULL);
+	for (int i = 0; i < threadNum; i++){
+		args[i] = (params*) malloc(sizeof(params));
+		args[i] -> lock = &lock;
+		args[i] -> multiple = mult * .25f;
+		args[i] -> isLock = isLock;
+		pthread_create(&threads[i], &threadAttrs, thread_func, (void*)args[i]);
+	}
+	for (int i = 0; i < threadNum; i++) {
+		pthread_join(threads[i], NULL);
+	}	
+	for (int i = 0; i < threadNum; i++) {
+		free(args[i]);
+	}
+	pthread_mutex_destroy(&lock);
+	gettimeofday(&stop, 0);
+	return (( stop.tv_sec-start.tv_sec)*1000000 + stop.tv_usec-start.tv_usec);
+}
+
+int main(int argc, char** argv){
+	fp=fopen("data.csv","w+");	
+	fprintf(fp,"Multiple, Synchronized, Thread Num, my_pthread Avg Time, Reg pthread Avg Time\n");
 	for (int mult = 1; mult <= 8; mult++){  
 		for (int isLock = 0; isLock <= 1; isLock++) {
 			for (int threadNum = 1; threadNum <= 10; threadNum++) {
-				float time = 0; 
+				float my_time = 0; 
+				float reg_time = 0; 
 				for (int numTimes = 1; numTimes <= totalTime; numTimes++) {
-					gettimeofday(&start, 0);
-					my_pthread_t threads[threadNum];
-					my_params* args[threadNum];
-					my_pthread_mutex_t lock;
-					my_pthread_mutex_init(&lock, NULL);
-					for (int i = 0; i < threadNum; i++){
-						args[i] = (my_params*) malloc(sizeof(my_params));
-						args[i] -> lock = &lock;
-						args[i] -> multiple = mult * .25f;
-						args[i] -> isLock = isLock;
-						my_pthread_create(&threads[i], NULL, my_thread_func, (void*)args[i]);
-					}
-					for (int i = 0; i < threadNum; i++) {
-						my_pthread_join(threads[i], NULL);
-					}	
-					for (int i = 0; i < threadNum; i++) {
-						free(args[i]);
-					}
-					my_pthread_mutex_destroy(&lock);
-
-					gettimeofday(&stop, 0);
-					time += (( stop.tv_sec-start.tv_sec)*1000000 + stop.tv_usec-start.tv_usec);
+					my_time += my_pthread_data();
+					reg_time += pthread_data();
 				}
-				time = time / (float)totalTime;
-				fprintf(fp,"%f, %d, %d, %f\n", .25f*mult, isLock, threadNum,time);
+				my_time = my_time / (float)totalTime;
+				reg_time = reg_time / (float)totalTime;
+				fprintf(fp,"%f, %d, %d, %f, %f\n", .25f*mult, isLock, threadNum,my_time, reg_time);
 			}
 		}
 	}
 	
 	fclose(fp);
-}
-
-void pthread_data() {
-	fp=fopen("pthread_data.csv","w+");	
-	fprintf(fp,"Multiple, Synchronized, Number of threads, Avg time across %d runs\n", totalTime);
-	for (int mult = 1; mult <= 8; mult++){  
-		for (int isLock = 0; isLock <= 1; isLock++) {
-			for (int threadNum = 1; threadNum <= 10; threadNum++) {
-				float time = 0; 
-				for (int numTimes = 1; numTimes <= totalTime; numTimes++) {
-					gettimeofday(&start, 0);
-					pthread_t threads[threadNum];
-					pthread_attr_t threadAttrs;
-					pthread_attr_init(&threadAttrs);
-					params* args[threadNum];
-					pthread_mutex_t lock;
-					pthread_mutex_init(&lock, NULL);
-					for (int i = 0; i < threadNum; i++){
-						args[i] = (params*) malloc(sizeof(params));
-						args[i] -> lock = &lock;
-						args[i] -> multiple = mult * .25f;
-						args[i] -> isLock = isLock;
-						pthread_create(&threads[i], &threadAttrs, thread_func, (void*)args[i]);
-					}
-					for (int i = 0; i < threadNum; i++) {
-						pthread_join(threads[i], NULL);
-					}	
-					for (int i = 0; i < threadNum; i++) {
-						free(args[i]);
-					}
-					pthread_mutex_destroy(&lock);
-
-					gettimeofday(&stop, 0);
-					time += (( stop.tv_sec-start.tv_sec)*1000000 + stop.tv_usec-start.tv_usec);
-				}
-				time = time / (float)totalTime;
-				fprintf(fp,"%f, %d, %d, %f\n", .25f*mult, isLock, threadNum,time);
-			}
-		}
-	}	
-	fclose(fp);
-}
-
-int main(int argc, char** argv){
-	pthread_data();
-	my_pthread_data();
 	return 0;
 }
