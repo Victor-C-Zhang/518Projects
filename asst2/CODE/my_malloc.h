116efaeb827c63f2a0b780bf3ca945e9c266c8b9
Bharti Mehta <bharu2999@gmail.com>
2021-04-13 18:10:39 -0400

    start of directmapping

--- asst2/CODE/my_malloc.h
+++ asst2/CODE/my_malloc.h
@@ -2,8 +2,28 @@
 #define ASST1_MY_MALLOC_H
 
 #include <stddef.h>
+#define THREADREQ 1
+#define LIBRARYREQ 0
+#define malloc(x) myallocate(x, __FILE__, __LINE__, THREADREQ)
+#define free(x) mydeallocate(x, __FILE__, __LINE__, THREADREQ)
+#define MEMSIZE 8388608
+#define NUMSEGMENTS 128;
+//metadata is represented as an unsigned short
+//it tells us the amount of bytes (following the metadata block) 
+//which are allocated for  user data
+//if the leftmost bit is 1, then the block is occupied
+//else it is free
+struct _metadata_ {
+	unsigned char size;
+} typedef metadata;
 
-void* myallocate(size_t size);
-void mydeallocate(void* ptr);
+struct _pagedata_ {
+	uint32_t pid; //-1 if free, else id of process using data
+	unsigned short p_ind; //where process thinks data is
+	unsigned short swapped_to; //where prior data was swapped to
+} typedef pagedata;
+
+void* myallocate(size_t size, char* file, int line, int threadreq);
+void mydeallocate(void* p, char* file, int line, int threadreq);
 
 #endif //ASST1_MY_MALLOC_H
