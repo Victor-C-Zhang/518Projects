944e66135d0e32fdd77caa7fbdb12b9d16677364
Victor Zhang <vcz4@scarletmail.rutgers.edu>
2021-04-19 23:43:25 -0400

    Add test-runner

--- asst2/CODE/test_runner.c
+++ asst2/CODE/test_runner.c
@@ -1,6 +1,14 @@
 #include "test_runner.h"
 
 int main() {
+//  testLinkedList();
+//  testHashMap();
+//  test_alarm();
+//  test_thread_create();
+//  testMutex();
+//  test_thread_create_join();
+//  test_thread_yield();
+
   TEST_create();
   TEST_get_put_delete();
 }
--- asst2/CODE/test_runner.h
+++ asst2/CODE/test_runner.h
@@ -5,6 +5,16 @@
 #ifndef ASST1_TEST_RUNNER_H
 #define ASST1_TEST_RUNNER_H
 
+// scheduler tests
+  void testLinkedList();
+  void testHashMap();
+  void test_alarm();
+  void test_thread_create();
+  void testMutex();
+  void test_thread_create_join();
+  void test_thread_yield();
+
+// malloc tests
 void TEST_create();
 void TEST_get_put_delete();
 
