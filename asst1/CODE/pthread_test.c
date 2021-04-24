f08e6524c8ae0a6dcb1b097697179b0fd364b2e6
Victor Zhang <vcz4@scarletmail.rutgers.edu>
2021-04-24 01:55:52 -0400

    Allow exit from main to kill entire program

--- asst1/CODE/pthread_test.c
+++ asst1/CODE/pthread_test.c
@@ -210,9 +210,9 @@ int main(int argc, char** argv){
 //  testLinkedList();
 //  testHashMap();
 //  test_alarm();
-  test_thread_create();
+//  test_thread_create();
 //  testMutex();
-  test_thread_create_join();
-//  test_thread_yield();
+//  test_thread_create_join();
+  test_thread_yield();
   return 0;
 }
